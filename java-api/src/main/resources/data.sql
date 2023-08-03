46564e25b42668844b322446039f3dc0f1178a40
ashleyclements <clementsa02@gmail.com>
2023-08-03 22:14:15 +0100

    book user database

--- java-api/src/main/resources/data.sql
+++ java-api/src/main/resources/data.sql
@@ -1,39 +1,25 @@
 --book database
 INSERT INTO book(book_id, name) VALUES (1,'trading_book_1');
-INSERT INTO book(book_id, name) VALUES (2,'trading_book_1');
-INSERT INTO book(book_id, name) VALUES (3,'trading_book_2');
-INSERT INTO book(book_id, name) VALUES (4,'trading_book_2');
-INSERT INTO book(book_id, name) VALUES (5,'trading_book_3');
-INSERT INTO book(book_id, name) VALUES (6,'trading_book_2');
-INSERT INTO book(book_id, name) VALUES (7,'trading_book_2');
-INSERT INTO book(book_id, name) VALUES (8,'trading_book_4');
-INSERT INTO book(book_id, name) VALUES (9,'trading_book_4');
-INSERT INTO book(book_id, name) VALUES (10,'trading_book_6');
-INSERT INTO book(book_id, name) VALUES (11,'trading_book_6');
-INSERT INTO book(book_id, name) VALUES (12,'trading_book_6');
-INSERT INTO book(book_id, name) VALUES (13,'trading_book_6');
-INSERT INTO book(book_id, name) VALUES (14,'trading_book_6');
-INSERT INTO book(book_id, name) VALUES (15,'trading_book_6');
-INSERT INTO book(book_id, name) VALUES (16,'trading_book_6');
-INSERT INTO book(book_id, name) VALUES (17,'trading_book_6');
-INSERT INTO book(book_id, name) VALUES (18,'trading_book_6');
-INSERT INTO book(book_id, name) VALUES (19,'trading_book_6');
-INSERT INTO book(book_id, name) VALUES (20,'trading_book_6');
-INSERT INTO book(book_id, name) VALUES (21,'trading_book_4');
-INSERT INTO book(book_id, name) VALUES (22,'trading_book_4');
-INSERT INTO book(book_id, name) VALUES (23,'trading_book_4');
-INSERT INTO book(book_id, name) VALUES (24,'trading_book_4');
-INSERT INTO book(book_id, name) VALUES (25,'trading_book_4');
-INSERT INTO book(book_id, name) VALUES (26,'trading_book_4');
-INSERT INTO book(book_id, name) VALUES (27,'trading_book_4');
-INSERT INTO book(book_id, name) VALUES (28,'trading_book_4');
+INSERT INTO book(book_id, name) VALUES (2,'trading_book_2');
+INSERT INTO book(book_id, name) VALUES (3,'trading_book_3');
+INSERT INTO book(book_id, name) VALUES (4,'trading_book_4');
+INSERT INTO book(book_id, name) VALUES (5,'trading_book_6');
+
 
 
 
 --users database
 INSERT INTO users(user_id, name, email, role) VALUES (1, 'Christopher Chan', 'Chris1@bank.com', 'Trader');
 INSERT INTO users(user_id, name, email, role) VALUES (2, 'Jimin Park', 'Jimin1@bank.com', 'Trader');
-INSERT INTO users(user_id, name, email, role) VALUES (3, 'Sarah Smith', 'SSmith2@bank.com', 'Trader');
-INSERT INTO users(user_id, name, email, role) VALUES (4, 'Sydney Leishman', 'SydneyLeish@bank.com', 'Trader');
-INSERT INTO users(user_id, name, email, role) VALUES (5, 'Mike Ciruzzi', 'MCur5@bank.com', 'Trader');
-INSERT INTO users(user_id, name, email, role) VALUES (6, 'Lisa Desai', 'lisa01@bank.com', 'Trader');
+INSERT INTO users(user_id, name, email, role) VALUES (3, 'Felix Lee', 'fLee@bank.com', 'Trader');
+
+
+--book_users
+INSERT INTO book_user(book_id, user_id) VALUES(1,1);
+INSERT INTO book_user(book_id, user_id) VALUES(2,1);
+INSERT INTO book_user(book_id, user_id) VALUES(3,1);
+INSERT INTO book_user(book_id, user_id) VALUES(1,2);
+INSERT INTO book_user(book_id, user_id) VALUES(1,3);
+INSERT INTO book_user(book_id, user_id) VALUES(4,3);
+INSERT INTO book_user(book_id, user_id) VALUES(4,2);
+INSERT INTO book_user(book_id, user_id) VALUES(5,2);
