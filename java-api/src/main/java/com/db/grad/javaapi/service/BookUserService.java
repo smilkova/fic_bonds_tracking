a37f771661b532a35dcbdd538adfb7195184c166
ashleyclements <clementsa02@gmail.com>
2023-08-03 20:38:38 +0100

    book service user

---
+++ java-api/src/main/java/com/db/grad/javaapi/service/BookUserService.java
@@ -0,0 +1,14 @@
+package com.db.grad.javaapi.service;
+import com.db.grad.javaapi.repository.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.db.grad.javaapi.model.Book;
+
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class BookUserService implements IBooksService{
+
+}
