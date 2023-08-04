66ceb6b23fe88013511398dbfbedcdbea18d98f5
ashleyclements <clementsa02@gmail.com>
2023-08-04 22:15:10 +0100

    updated securities and all danche's edits

--- java-api/src/main/java/com/db/grad/javaapi/repository/SecuritiesRepository.java
+++ java-api/src/main/java/com/db/grad/javaapi/repository/SecuritiesRepository.java
@@ -7,11 +7,11 @@ import org.springframework.data.jpa.repository.Query;
 import java.util.List;
 
 public interface SecuritiesRepository extends JpaRepository<Securities, Integer> {
-    @Query(nativeQuery = true,
-            value = "select * from securities where " +
-                    "CAST(mature_date AS int) >= DATE_SUB(:CAST(today_date AS int), INTERVAL 5 DAY) and" +
-                    "CAST(mature_date AS int) <= DATE_ADD(:CAST(today_date AS int), INTERVAL 5 DAY)")
-    List<Securities> findByRecentAndNearMaturity(String today_date);
+//    @Query(nativeQuery = true,
+//            value = "select * from securities where " +
+//                    "CAST(mature_date AS int) >= DATE_SUB(:CAST(today_date AS int), INTERVAL 5 DAY) and" +
+//                    "CAST(mature_date AS int) <= DATE_ADD(:CAST(today_date AS int), INTERVAL 5 DAY)")
+//    List<Securities> findByRecentAndNearMaturity(String today_date);
 
     @Query(nativeQuery = true,
             value = "select * from securities where " +
